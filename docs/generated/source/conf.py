2864e5390f3ea935121d09c7e3f9135f87d20130
Benjamin Jordan <bmj465@gmail.com>
2017-02-26 23:22:04 -0800

    More minute changes.

--- docs/generated/source/conf.py
+++ docs/generated/source/conf.py
@@ -4,7 +4,7 @@ import sys
 import subprocess, os
 read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 if read_the_docs_build:
-    subprocess.call('cd ../../..;doxygen -g .doxygen.config', shell=True)
+    subprocess.call('cd ../../..; doxygen .doxygen.config', shell=True)
 
 # theme
 import sphinx_rtd_theme
