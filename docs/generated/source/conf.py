d696079599a09dec035f11d336a06779606b2bf9
Benjamin Jordan <bmj465@gmail.com>
2017-02-26 21:11:09 -0800

    ReadTheDocs integration.

--- docs/generated/source/conf.py
+++ docs/generated/source/conf.py
@@ -1,5 +1,11 @@
 import sys
 
+# run doxygen
+import subprocess, os
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+if read_the_docs_build:
+    subprocess.call('cd ../doxygen; doxygen', shell=True)
+
 # theme
 import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
